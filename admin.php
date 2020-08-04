92b8e97ce7550777c57a3384feba07ced2864bd5
Samuel Ochiwu <samuel36991977@gmail.com>
2020-08-04 13:09:05 +0100

    admin.php added

--- admin.php
+++ admin.php
@@ -41,7 +41,7 @@ include 'header.php';
     <!-- Main content -->
     <section class="content">
       <div class="container-fluid">
-        <!-- Small boxes (Stat box) -->
+       
         <div class="row">
           <div class="col-lg-3 col-6">
             <!-- small box -->
